f0c78ef8e784be5f61e25304b0b8f92bee6dc46e
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-10-18 15:41:03 +0300

    Initial commit

---
+++ lib/jwt/rack.rb
@@ -0,0 +1,8 @@
+require "jwt/rack/version"
+
+module Jwt
+  module Rack
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/jwt/rack_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jwt::Rack do
+  it "has a version number" do
+    expect(Jwt::Rack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
